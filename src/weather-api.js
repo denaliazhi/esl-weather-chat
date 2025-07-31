61e8ed6f7c283b00a4451f94c745d517cdd37d9d
Denalia Zhi <d.naliazhi@gmail.com>
2025-07-31 14:28:30 -0400

    Write weather-api module to get data for given location and date range

---
+++ src/weather-api.js
@@ -0,0 +1,37 @@
+/**
+ * This module gets weather data for a given
+ * location from https://www.visualcrossing.com/
+ */
+
+import moment from "moment";
+
+// Free API key exposed in this project
+const key = "JWBFXXUEHSFZ6CMGKQR6SFNKQ";
+
+async function getWeatherData(loc) {
+  const [startDate, endDate] = getDateRange();
+
+  try {
+    const resp = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${loc}/${startDate}/${endDate}?unitGroup=us&include=days&key=${key}&contentType=json`
+    );
+    const data = await resp.json();
+    return data;
+  } catch (err) {
+    return "Error: Weather data not found.";
+  }
+}
+
+function getDateRange() {
+  let start = new Date();
+  start.setDate(start.getDate() - 1);
+  start = moment(start).format("YYYY-MM-DD");
+
+  let end = new Date();
+  end.setDate(end.getDate() + 1);
+  end = moment(end).format("YYYY-MM-DD");
+
+  return [start, end];
+}
+
+export default getWeatherData;
